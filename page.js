a0070eed9d9523b2cbe13ebc45c97306758920a9
AusFernando <Dacrazed@gmail.com>
2025-07-17 18:24:38 +1000

    Adding local test data, building presence mapmode

--- page.js
+++ page.js
@@ -1,6 +1,7 @@
 const data_url = "https://script.google.com/macros/s/AKfycbxsrSih1KNt_B6lHGjjCPLA6PBLUgpSMbpELJ1iFNI6XVVY288zYI1E6H-FK9SzOW5Zqg/exec";
 var data;
 var currentSelect = null;
+var currentMapMode = "";
 var highestWealth = -1;
 var lowestWealth = 1000;
 var highestTaxation = -1
@@ -11,6 +12,9 @@ var highestUnrest = -1;
 var lowestUnrest = 1000;
 var highestSlotsRemaining = -1;
 var lowestSlotsRemaining = 1000;
+var buildingOwnerList = new Set();
+var buildingParseRegex = /^(?<building_name>[A-Za-z\s]*) \- (?<stats>((?:We|Tx|U|In|\$|Tx|Sv|Sc|Pr|Tr)\:\-?\d{1,3}(?:\.\d+)?(?: (\(\-?\d{1,3}(\.\d{1,3})?)\))?\,* *)+)(?<owner>\([a-zA-Z\s]*\))$/
+var buildingOwnerStatistics = {};
 
 var defaultColours = {};
 var ownerColours = {
@@ -42,15 +46,22 @@ function escapeHtml(unsafe) {
   }
 
 async function loadData() {
-	data_response = await fetch(data_url, { method: "GET" });
-	data_json = await data_response.json();
-	console.log(data_json);
+	var params = new URL(location.toString());
+	if (_local_test_data != undefined && _local_test_data != null) {
+		data_json = _local_test_data;
+		console.log("local test data was loaded.")
+	}	
+	else {
+		data_response = await fetch(data_url, { method: "GET" });
+		data_json = await data_response.json();
+		console.log(data_json);
+	}
 
 	headers = data_json.data[0];
 	var structured = [];
 
 	for(var i = 0; i < data_json.data.length; i++) {
-		structured.push({
+		var n_data = {
 			"ID": data_json.data[i][0],
 			"Name": data_json.data[i][1],
 			"Owner": data_json.data[i][2],
@@ -66,41 +77,43 @@ async function loadData() {
 			"Rural_Buildings": data_json.data[i][12],
 			"Urban_Buildings": data_json.data[i][13],
 			"Resource_Buildings": data_json.data[i][14],
-		})
+			"Building_Owners": new Set(),
+			"Building_Owner_Count": {}
+		}
 
 		// Generate statistics.
-		if (highestWealth < data_json.data[i][3]) {
-			highestWealth = data_json.data[i][3];
+		if (highestWealth < n_data.Wealth) {
+			highestWealth = n_data.Wealth;
 		}
-		if (lowestWealth > data_json.data[i][3]) {
-			lowestWealth = data_json.data[i][3];
-		}
-
-		if (highestTaxation < data_json.data[i][4]) {
-			highestTaxation = data_json.data[i][4];
-		}
-		if (lowestTaxation > data_json.data[i][4]) {
-			lowestTaxation = data_json.data[i][4];
+		if (lowestWealth > n_data.Wealth) {
+			lowestWealth = n_data.Wealth;
 		}
 
-		if (highestSecurity < data_json.data[i][5]) {
-			highestSecurity = data_json.data[i][5];
+		if (highestTaxation < n_data.Taxation) {
+			highestTaxation = n_data.Taxation;
 		}
-		if (lowestSecurity > data_json.data[i][5]) {
-			lowestSecurity = data_json.data[i][5];
-		}
-
-		if (highestUnrest < data_json.data[i][6]) {
-			highestUnrest = data_json.data[i][6];
-		}
-		if (lowestUnrest > data_json.data[i][6]) {
-			lowestUnrest = data_json.data[i][6];
+		if (lowestTaxation > n_data.Taxation) {
+			lowestTaxation = n_data.Taxation;
 		}
 
-		SlotsTotal = data_json.data[i][9] + data_json.data[i][10];
-		SlotsTotal -= String(data_json.data[i][12]).split("\n").length;
-		SlotsTotal -= String(data_json.data[i][13]).split("\n").length;
-		structured[i].Slots_Remaining = SlotsTotal;
+		if (highestSecurity < n_data.Security) {
+			highestSecurity = n_data.Security;
+		}
+		if (lowestSecurity > n_data.Security) {
+			lowestSecurity = n_data.Security;
+		}
+
+		if (highestUnrest < n_data.Unrest) {
+			highestUnrest = n_data.Unrest;
+		}
+		if (lowestUnrest > n_data.Unrest) {
+			lowestUnrest = n_data.Unrest;
+		}
+
+		SlotsTotal = n_data.Rural_Slots + n_data.Urban_Slots;
+		SlotsTotal -= String(n_data.Rural_Buildings).split("\n").length;
+		SlotsTotal -= String(n_data.Urban_Buildings).split("\n").length;
+		n_data.Slots_Remaining = SlotsTotal;
 
 		if (highestSlotsRemaining < SlotsTotal) {
 			highestSlotsRemaining = SlotsTotal;
@@ -108,6 +121,57 @@ async function loadData() {
 		if (lowestSlotsRemaining > SlotsTotal) {
 			lowestSlotsRemaining = SlotsTotal;
 		}
+
+		if (i > 0) {
+			RuralSplit = n_data.Rural_Buildings.split("\n");
+			for (const building of RuralSplit) {
+				ParseBuildingsRural = building.match(buildingParseRegex);
+				if (ParseBuildingsRural == null) {
+					console.log(`id: ${n_data.ID}: ${building}`);
+					continue;
+				}
+				buildingOwnerList.add(ParseBuildingsRural.groups.owner);
+				n_data.Building_Owners.add(ParseBuildingsRural.groups.owner);
+				if (n_data.Building_Owner_Count[ParseBuildingsRural.groups.owner] == null) {
+					n_data.Building_Owner_Count[ParseBuildingsRural.groups.owner] = 1;
+				}
+				else {
+					n_data.Building_Owner_Count[ParseBuildingsRural.groups.owner]++;
+				}
+			}
+			UrbanSplit = n_data.Urban_Buildings.split("\n");
+			for (const building of UrbanSplit) {
+				ParseBuildingsRural = building.match(buildingParseRegex);
+				if (ParseBuildingsRural == null) {
+					console.log(`id: ${n_data.ID}: ${building}`);
+					continue;
+				}
+				buildingOwnerList.add(ParseBuildingsRural.groups.owner);
+				n_data.Building_Owners.add(ParseBuildingsRural.groups.owner);
+				if (n_data.Building_Owner_Count[ParseBuildingsRural.groups.owner] == null) {
+					n_data.Building_Owner_Count[ParseBuildingsRural.groups.owner] = 1;
+				}
+				else {
+					n_data.Building_Owner_Count[ParseBuildingsRural.groups.owner]++;
+				}
+			}
+		}
+
+		for (const owner in n_data.Building_Owner_Count) {
+			if (buildingOwnerStatistics[owner] == null) {
+				buildingOwnerStatistics[owner] = {
+					"lowest": 0,
+					"highest": n_data.Building_Owner_Count[owner]
+				}
+			}
+			else {
+				if (n_data.Building_Owner_Count[owner] > buildingOwnerStatistics[owner].highest) {
+					buildingOwnerStatistics[owner].highest = n_data.Building_Owner_Count[owner];
+				}
+			}
+		}
+
+		structured.push(n_data);
 	}
 
 	return structured;
@@ -124,6 +188,41 @@ loadData().then(
 		document.getElementById("dimScreen").remove();
 		data = value; 
 		var buttons = document.getElementsByClassName('mm_button');
+
+		const owoDiv = document.getElementById("mm_buildings_owner_list");
+		let isFirst = true;
+		for (const owner of buildingOwnerList) {
+			
+
+			let new_radio = document.createElement("input");
+			new_radio.setAttribute("name", "building_presence");
+			new_radio.setAttribute("type", "radio");
+			new_radio.setAttribute("value", owner);
+			new_radio.setAttribute("id", `build_presence_${owner}`)
+			if (isFirst) {
+				new_radio.setAttribute("checked", "");
+				isFirst = false;
+			}
+			new_radio.addEventListener(
+				"click", (event) => {
+					if (currentMapMode == "building_owner") {
+
+						doBuildingsPresenceMapmode();
+					}
+				}
+			)
+
+			let new_label = document.createElement("label");
+			new_label.setAttribute("for", `build_presence_${owner}`);
+			new_label.innerText=owner;
+
+
+			let new_list_item = document.createElement("li");
+			new_list_item.append(new_radio);
+			new_list_item.append(new_label);
+			owoDiv.appendChild(new_list_item);
+		}
+
 		// Enable buttons :)
 		for(var i = 0; i < buttons.length; i++) {
 			buttons[i].disabled = false;
@@ -188,10 +287,14 @@ function showMapMode(mapmode) {
 		case "slots_remaining":
 			doSlotsRemainingMapMode();
 			break;
+		case "building_owner":
+			doBuildingsPresenceMapmode();
+			break;
 		default:
 			alert(`No map mode named ${mapmode}.`);
-			break;
+			return;
 	}
+	currentMapMode = mapmode;
 }
 
 function doWealthMapMode() {
@@ -292,6 +395,27 @@ if (lowestSlotsRemaining == highestSlotsRemaining) {
 		region_data = data[r];
 		diffFromHighest = (highestSlotsRemaining - region_data.Slots_Remaining) / range;
 		diffFromLowest =  (region_data.Slots_Remaining - lowestSlotsRemaining) / range;
+		Green = 255 * diffFromLowest;
+		Red = 255 * diffFromHighest;
+		svgDoc.getElementById(`region_${r}`).style.fill = `rgba(${Red}, ${Green}, 0, 0.33)`
+	}
+}
+
+function doBuildingsPresenceMapmode() {
+	let selectedOwner = document.getElementById("mm_building_owner_form").elements["building_presence"].value;
+	let statistic = buildingOwnerStatistics[selectedOwner];
+	if (statistic.lowest == statistic.highest) {
+		for (let r = 1; r < 23; r++) {
+			svgDoc.getElementById(`region_${r}`).style.fill = "#ffff0054";
+		}
+		return;
+	}
+	range = statistic.highest - statistic.lowest;
+	for (let r = 1; r < 23; r++) {
+		region_data = data[r];
+		let c = region_data.Building_Owner_Count[selectedOwner] ?? 0;
+		diffFromHighest = (statistic.highest - c) / range;
+		diffFromLowest =  (c - statistic.lowest) / range;
 		Green = 255 * diffFromLowest;
 		Red = 255 * diffFromHighest;
 		svgDoc.getElementById(`region_${r}`).style.fill = `rgba(${Red}, ${Green}, 0, 0.33)`
